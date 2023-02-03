91232f464cd3f20ff6a71c173a47b717c13ed0b3
Samurai-way <olegmoishevych@gmail.com>
2023-02-03 21:45:11 +0200

    feat: REST API, (get)

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,15 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-const videos = [
-    {id: 0, title: 'YouTube', author: 'Oleg'},
-    {id: 1, title: 'Cinema', author: 'Artur'},
-    {id: 2, title: 'Change', author: 'Petya'},
+const persons = [
+    {name: 'Dima', age: 10},
+    {name: 'Artur', age: 22},
+    {name: 'Oleg', age: 25},
 ]
 
-app.get('/videos', (req, res) => {
-    if (videos) {
-        res.send(videos)
-    } else {
-        res.send(404)
-    }
+app.get('/persons', (req, res) => {
+    res.send(persons)
 })
-
-app.post('/videos', (req, res) => {
-    debugger
-    const video = req.body
-    videos.push(video)
-    if (videos) {
-        res.send(videos)
-    } else {
-        res.send(404)
-    }
-})
-
-app.delete('/videos/:id', (req, res) => {
-    const deleteVideo = videos.filter((v) => v.id === req.body.id)
-    if(videos){
-        res.send(deleteVideo)
-    }
-    res.send(404)
-})
-
-const a = 10
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
