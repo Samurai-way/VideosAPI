20efaa6d9ad5da13fc8fcdfdc2e5f4ae4115c448
Samurai-way <olegmoishevych@gmail.com>
2023-02-03 21:51:38 +0200

    feat: REST API, url param(id)

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,32 @@ const app = express()
 const port = 3000
 
 const persons = [
-    {name: 'Dima', age: 10},
-    {name: 'Artur', age: 22},
-    {name: 'Oleg', age: 25},
+    {id: 1, name: 'Dima', age: 10},
+    {id: 2, name: 'Artur', age: 22},
+    {id: 3, name: 'Oleg', age: 25},
 ]
 
 app.get('/persons', (req, res) => {
     res.send(persons)
 })
 
-app.get('/persons/:personName', (req, res) => {
-    const person = persons.find(p => p.name === req.params.personName)
-    if(person){
-        res.send(person)
+// app.get('/persons/:personName', (req, res) => {
+//     const person = persons.find(p => p.name === req.params.personName)
+//     if(person){
+//         res.send(person)
+//     }else {
+//         res.send(404)
+//     }
+// })
+
+app.get('/persons/:id', (req, res) => {
+    const personId = persons.find(p => p.id === +req.params.id)
+    if(personId){
+        res.send(personId)
     }else {
         res.send(404)
     }
 })
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
