a18b17a209d85f605ee1892b26ee26118269a9af
Samurai-way <olegmoishevych@gmail.com>
2023-02-04 11:41:18 +0200

    feat: videos, get, post

--- src/routers/videos-router.ts
+++ src/routers/videos-router.ts
@@ -1,4 +1,34 @@
-import {Router} from "express";
+import {Request, Response, Router} from "express";
 
 export const videosRouter = Router({})
 
+const videos = [
+    {
+        id: 0,
+        title: "string",
+        author: "string",
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: "2023-02-04T09:22:50.102Z",
+        publicationDate: "2023-02-04T09:22:50.102Z",
+        availableResolutions: [
+            "P144"
+        ]
+    }
+]
+
+videosRouter.get('/', (req: Request, res: Response) => {
+    res.status(200).send(videos)
+})
+videosRouter.post('/', (req: Request, res: Response) => {
+    const title = req.body.title
+    const author = req.body.author
+    const newVideo = {
+        id: (new Date().toISOString()),
+        title: title,
+        author: author,
+        availableResolutions: [
+            "P144"
+        ]
+    }
+})
