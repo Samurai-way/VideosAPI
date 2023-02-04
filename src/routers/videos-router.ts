b7ea66d86e637b941d8b25c3a0ba34b0480e1863
Samurai-way <olegmoishevych@gmail.com>
2023-02-04 13:56:01 +0200

    fix: post, get

--- src/routers/videos-router.ts
+++ src/routers/videos-router.ts
@@ -2,15 +2,26 @@ import {Request, Response, Router} from "express";
 
 export const videosRouter = Router({})
 
-const videos = [
+export type RootObject = {
+    id: number;
+    title: string;
+    author: string;
+    canBeDownloaded: boolean;
+    minAgeRestriction?: any;
+    createdAt: Date;
+    publicationDate: Date;
+    availableResolutions: string[];
+}
+
+const videos: RootObject[] = [
     {
         id: 0,
         title: "string",
         author: "string",
         canBeDownloaded: true,
         minAgeRestriction: null,
-        createdAt: "2023-02-04T09:22:50.102Z",
-        publicationDate: "2023-02-04T09:22:50.102Z",
+        createdAt: new Date(),
+        publicationDate: new Date(),
         availableResolutions: [
             "P144"
         ]
@@ -21,28 +32,47 @@ videosRouter.get('/', (req: Request, res: Response) => {
     res.status(200).send(videos)
 })
 videosRouter.post('/', (req: Request, res: Response) => {
-    const {title, author} = req.body
+    const {title, author, availableResolutions} = req.body
     const availableResolutionsArray = ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"]
-    const availableValidation = availableResolutionsArray.some(el => availableResolutionsArray.indexOf(el) == -1)
-    const newVideo = {
-        id: (new Date().toISOString()) as any,
-        title: title as any,
-        author: author as any,
+    const availableValidation = availableResolutions.every((el: any) => availableResolutionsArray.includes(el))
+    console.log(availableResolutions)
+    console.log(availableValidation)
+    const newVideo: RootObject = {
+        id: +(new Date()),
+        title,
+        author,
         canBeDownloaded: true,
         minAgeRestriction: null,
-        createdAt: (Date.now()) as any,
-        publicationDate: (Date.now()) as any,
-        availableResolutions: [req.body.availableResolutions]
+        createdAt: new Date(),
+        publicationDate: new Date(),
+        availableResolutions: availableResolutions
 
     }
-    if (!title || title.length > 40 || !author || author.length > 20 || !availableValidation) {
+    const arr = []
+    if (!title || title.length > 40) {
+        const exeption = {
+            message: "error",
+            field: "title"
+        }
+        arr.push(exeption)
+    }
+    if (!author || author.length > 20) {
+        const exeption = {
+            message: "error",
+            field: "author"
+        }
+        arr.push(exeption)
+    }
+    if (!availableValidation) {
+        const exeption = {
+            message: "error",
+            field: "availableResolutions"
+        }
+        arr.push(exeption)
+    }
+    if (arr.length > 0) {
         res.status(400).send({
-            errorsMessages: [
-                {
-                    message: "error",
-                    field: "error"
-                }
-            ]
+            errorsMessages: arr
         })
     }
     videos.push(newVideo)
